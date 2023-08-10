7d7bb5f44fd4acdb423c97ea6b29bfd8f9221b8b
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-08-10 13:00:49 +0100

    Added page loading fallback

--- explorer/src/App.vue
+++ explorer/src/App.vue
@@ -2,6 +2,15 @@
   <component :is="layout">
     <Suspense>
       <router-view />
+
+      <template #fallback>
+        <div class="fixed top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center">
+          <div class="flex items-center gap-4">
+            <LoadingIcon class="animate-spin" />
+            <span class="text-helper">Loading...</span>
+          </div>
+        </div>
+      </template>
     </Suspense>
   </component>
 </template>
@@ -11,6 +20,7 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import HomeLayout from './layouts/Home.vue'
 import DefaultLayout from './layouts/Default.vue'
+import LoadingIcon from './components/LoadingIcon.vue'
 
 const route = useRoute()
 
