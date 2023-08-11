da92a78118ca10d1a3053c3840170ce652f5d1a0
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-08-11 10:19:14 +0100

    Use shiki cdn

--- explorer/src/shiki.ts
+++ explorer/src/shiki.ts
@@ -1,6 +1,6 @@
 import { getHighlighter, setCDN } from 'shiki'
 
-setCDN('/shiki')
+setCDN('https://cdn.jsdelivr.net/npm/shiki')
 
 export const highlighter = await getHighlighter({
   theme: 'github-dark-dimmed',
