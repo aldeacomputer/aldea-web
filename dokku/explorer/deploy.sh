8df0732c09849b9d6f94a1c51b464fe4ca7f290d
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-08-11 09:34:33 +0100

    dokku deploy script

---
+++ dokku/explorer/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GIT_REMOTES=`git remote | grep dokku-explorer`
+
+if [[ -z "$GIT_REMOTES" ]]
+then
+  git remote add dokku-explorer dokku@explorer.aldea.computer:explorer
+fi
+
+REPO_CHANGES=`git status -s`
+echo $REPO_CHANGES
+if [[ ! -z $REPO_CHANGES ]]
+then
+  echo "please commit all the changes before deploy"
+  exit 1
+fi
+
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+if [[ $CURRENT_BRANCH != 'main' ]]
+then
+  echo "only main branch can be deployed"
+  exit 1
+fi
+
+git push dokku-explorer main
