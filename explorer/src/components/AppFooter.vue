176bf678492fae3294b0a9a6199678287c9051ca
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-08-24 12:04:20 +0100

    app footer added

---
+++ explorer/src/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="flex items-center justify-between gap-4 px-4 bg-layer-01">
+    <div class="flex flex-col gap-2 py-4">
+      <RouterLink to="/">
+        <img src="/aldea-logo-full.svg" width="96" alt="Aldea Explorer" />
+      </RouterLink>
+      <p class="text-12 text-secondary">Copyright © 2023 Run Computer Company</p>
+    </div>
+    <div class="py-4">
+      <ul class="flex items-center gap-2 md:gap-4 lg:gap-6 text-24">
+        <li v-for="{icon, url} of socialLinks">
+          <a
+            :href="url"
+            target="_blank"
+            class="text-secondary hover:text-white transition-colors">
+            <component :is="icon" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { CaLogoGithub, CaLogoLinkedin, CaLogoTwitter } from '@kalimahapps/vue-icons'
+
+const socialLinks = [
+  { icon: CaLogoTwitter, url: 'https://twitter.com/aldeacomputer' },
+  { icon: CaLogoLinkedin, url: 'https://www.linkedin.com/company/aldea-computer/' },
+  { icon: CaLogoGithub, url: 'https://github.com/orgs/aldeacomputer' },
+]
+</script>
--- explorer/src/layouts/Default.vue
+++ explorer/src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pt-14 md:pt-20 lg:pt-28">
+  <div class="pt-14 md:pt-20 lg:pt-28 md:pb-4 lg:pb-12">
     <AppHeader class="fixed top-0 left-0 right-0 z-40 shadow-md" />
     <main class="px-4 py-6 sm:p-6 md:px-8 lg:px-10">
       <div class="max-w-6xl mx-auto">
@@ -8,11 +8,14 @@
     </main>
     <SearchModal v-if="search.show" />
   </div>
+
+  <AppFooter />
 </template>
 
 <script setup lang="ts">
 import { useSearchStore } from '../stores/search'
 import AppHeader from '../components/AppHeader.vue'
+import AppFooter from '../components/AppFooter.vue'
 import SearchModal from '../components/SearchModal.vue'
 
 const search = useSearchStore()
